4323573d9fd5f044caab4c8e9a9f0e50b41e5fa9
Luis Funes <Luis.Funes86@hotmail.com>
2023-03-21 01:13:08 -0300

    Primer Commit, hay que hacerlo responsive en celulares

---
+++ Serendipia/js/boton_hamburguesa.js
@@ -0,0 +1,15 @@
+function btnHambur() {
+
+    var hambur = document.getElementById('btnes_scroll')
+      
+    if (hambur.style.display == 'block' ) {
+
+        hambur.style.display = 'none'
+    }
+    else {
+        hambur.style.display = 'block'
+    }
+}
+
+
+
---
+++ Serendipia/js/unidad7.js
@@ -0,0 +1,29 @@
+/*let nombre = prompt ('¿Cómo te llamas?')
+
+alert(`bienvenid@ a Serendipia ${nombre}`)
+
+let coeficiente = 0.9
+let titulopeso = ('Tu peso ideal')
+let peso = prompt ('¿Cuánto pesas actualmente?(Kg)')
+let altura = prompt ('¿Cuál es tu altura en cm?')
+let edad = prompt ('¿Qué edad tienes?')
+let pesoideal = altura-100+((edad/10)*coeficiente)
+
+*/
+function correr() {
+    var salirOno = document.getElementById(pesoideal)
+
+        if (peso>pesoideal+5) {
+
+            alert('tienes que ejercitarte más!!')
+            
+        }
+        if (peso<pesoideal-10) {
+
+            alert('debes alimentarte mejor')
+        }
+        if (peso<=pesoideal+5 || peso>=pesoideal-5)
+        
+        alert('sigue así')
+
+}
